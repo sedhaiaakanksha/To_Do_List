71fe6f7dedfe726f646213e543bf49f192a0347a
Aakanksha <sedhaiaakanksha@gmail.com>
2026-03-30 23:36:11 +0545

    rendering the tasks

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ document.addEventListener("DOMContentLoaded", () => {
   const toDoList = document.getElementById("todo-list");
 
   let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
-  tasks.forEach((task) => renderTask());
+  tasks.forEach((task) => renderTask(task));
 
   addTaskBtn.addEventListener("click", function () {
     const taskText = toDoInput.value.trim();
@@ -21,7 +21,23 @@ document.addEventListener("DOMContentLoaded", () => {
   });
 
   function renderTask(task) {
-    console.log(task);
+    const li = document.createElement("li");
+    li.setAttribute("data-id", task.id);
+    if (task.completed) li.classList.add("completed");
+    li.innerHTML = `
+  <span>${task.text}</span>
+  <button>delete</button>
+  `;
+    toDoList.appendChild(li);
+
+    li.addEventListener("click", (e) => {
+      if (e.target.tagName == "BUTTON") return;
+      task.completed = !task.completed;
+      li.classList.toggle("completed");
+      saveTask();
+    });
+
+    console.log("working");
   }
   function saveTask() {
     localStorage.setItem("tasks", JSON.stringify(tasks));
